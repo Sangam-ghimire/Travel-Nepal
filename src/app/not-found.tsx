c72114003fc760003dd3ecb571dddeef258ded1d
Sangam Ghimire <1sangamghimire1@gmail.com>
2023-09-06 16:13:08 +0545

    not found page added, but warning with inline css exists

---
+++ src/app/not-found.tsx
@@ -0,0 +1,22 @@
+//not-found page
+"use client"
+import Image from 'next/image';
+
+export default function NotFound() {
+  const imageStyle = {
+    display:"flex",
+    justifyContent:"center",
+    marginTop:"20%"
+
+  };
+    return (
+      <div style={imageStyle}>
+        <Image 
+        src="/not_found.png"
+        width={500}
+        height={403.44}
+        alt="Not Found"
+        />
+      </div>
+    );
+}
