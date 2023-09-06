e9785746174574381b7550a561d7ca22cf488898
Sangam Ghimire <1sangamghimire1@gmail.com>
2023-09-06 21:05:25 +0545

    site/page.tsx rectification

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,58 +1,29 @@
-import locateStyle from "./page.module.css";
-import ButtonFollow from "./location/[id]/components/ButtonFollow";
-import ButtonAddPost from "./location/[id]/components/ButtonAddPost";
-import Posts from "./location/[id]/components/Posts";
-import {TbLocationFilled }from "react-icons/tb"
-import { notFound } from "next/navigation";
+"use client";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+import mainStyles from "./page.module.css";
+import PostFeed from "./components/PostFeed/PostFeed";
 
-async function fetchLocationData(id: string) {
-  const res: Response = await fetch(
-    `https://ap-south-1.aws.data.mongodb-api.com/app/trek-diaries-bmymy/endpoint/fetchLocaitonData?id=${id}`,
-    { cache: "no-store" }
-  );
-  
-  if(!res.ok) return undefined
-
-  return res.json();
-}
-
-export default async function LocationPage({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const locationID: string = params.id;
-  const data = await fetchLocationData(locationID);
-
-  if (!data) {
-    notFound()
-  }
+export default function Home() {
+  const router = useRouter();
+  const session = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push("/login");
+    },
+  });
 
   return (
-    <div className={locateStyle.wrapper}>
-      <div className={locateStyle.left}></div>
-      <div className={locateStyle.center}>
-        <div className={locateStyle.cTop}>
-            <div className={locateStyle.header}>
-              <div className={locateStyle.hText}>
-                <div className={locateStyle.locName}>
-                <TbLocationFilled size={28}/>
-                <h1>{data.address}</h1>
-                </div>
-                <p>{data.description}</p>
-              </div>
-              <div className={locateStyle.hButton}>
-                <ButtonAddPost locationID={locationID} />
-                <ButtonFollow locationID={locationID} />
-              </div>
-            </div>
-        </div>
-
-        <div className={locateStyle.cBottom}>
-          <Posts locationId={params.id} />
+    <div className={mainStyles.wrapper}>
+      <div className={mainStyles.left}></div>
+      <div className={mainStyles.center}>
+        <div className={mainStyles.post}>
+          {session.status === "authenticated" && (
+            <PostFeed email={session.data?.user?.email as string} />
+          )}
         </div>
       </div>
-      <div className={locateStyle.left}></div>
+      <div className={mainStyles.right}></div>
     </div>
   );
 }
