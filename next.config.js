bd34768b812346259a3e8e655d211985ebc4ab8d
Sangam Ghimire <1sangamghimire1@gmail.com>
2023-09-06 09:10:34 +0545

    cloudinary fetching mentioned

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+/** @type {import('next').NextConfig} */ //JSDoc comment for type checking
+const nextConfig = {
+    images: {
+        domains: ['res.cloudinary.com'], // allows images to be fetched from res.cloudinary domain
+    }
+}
 
 module.exports = nextConfig
