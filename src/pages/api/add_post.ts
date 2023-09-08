03112a5df825ec081709c43cfb8d579ae3e85673
Sangam Ghimire <1sangamghimire1@gmail.com>
2023-09-08 16:09:27 +0545

    documenting add post

--- src/pages/api/add_post.ts
+++ src/pages/api/add_post.ts
@@ -4,19 +4,23 @@ import Post from "../../../lib/modals/Post";
 import Location from "../../../lib/modals/Location";
 import User from "../../../lib/modals/User"
 
+//here we are defining the handler function to handle the request and response for the api of add_post
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if(req.method === 'POST') {
         try {
+            //here we are getting the description, locationId, image_URL, userId from the request body
             const {Description, locationId, image_URL, userId, TrailCondition, Weather, Accessibility, overallScore} = req.body as any;
             if (await dbConnect()) // connect to database
             {
                 console.log("Connection established in addPost");
             }
+            //here we are checking if the location already exists or not
             const user: any = await User.findOne({email: userId});
             const fullName = user.name;
-
+            //if location already exists then we are returning the response with status code 409
             const location: any = await Location.findById(locationId); // importing address from location
             const address = location.address;
+            
             if(!address)
             {
                 return res.status(422).json({
@@ -37,11 +41,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             newPost.rating.Accessibility = Accessibility;
             newPost.rating.overallScore = overallScore;
             await newPost.save();
-
-            return res.status(201).json({ success: true, data: newPost })
+            //here we are saving the post to the database and returning the response with status code 201
+            return res.status(201).json({ success: true, data: newPost });
 
         } catch (error) {
             console.log(error);
+            //if any error occurs then we are returning the response with status code 500
             return res.status(400).json({ success: false })
         }
     }
