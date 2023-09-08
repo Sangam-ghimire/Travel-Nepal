aeb33d50a1aed5b4fa96eaf972eaa807cd5fccd2
Sangam Ghimire <1sangamghimire1@gmail.com>
2023-09-08 16:26:33 +0545

    documentation of metadata section in signup page done

--- src/app/(auth)/sign_up/page.tsx
+++ src/app/(auth)/sign_up/page.tsx
@@ -2,11 +2,13 @@ import SignUpForm from "./components/SignUpForm"
 import signupStyles from "./page.module.css";
 import type { Metadata } from "next";
 
+//metadata for the page for SEO
 export const metadata:Metadata = {
   title: "Sign Up | TrekDiaries",
   description: "Sign up page of TrekDiaries"
 }
 
+//here the function is exported and this is used to display the sign up page
 export default function Page() {
   return (
     <div className={signupStyles.app}>
