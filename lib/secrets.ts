3fce4a7c5d297a5980f9dc08e6bf41008cf5f2c6
Sangam Ghimire <1sangamghimire1@gmail.com>
2023-09-08 16:03:02 +0545

    secrets.ts documentation done

--- lib/secrets.ts
+++ lib/secrets.ts
@@ -1,10 +1,12 @@
 // Desc: This file contains functions to get secrets from the environment.
 export const getAuthSecret = (): string => {
+    // Get the secret from the environment.
     const secret = process.env.NEXTAUTH_SECRET
 
+    // If the secret is not set, throw an error.
     if (!secret || secret.length === 0) {
         throw new Error("The environment variable NEXTAUTH_SECRET is not set.")
     }
-
+    // Return the secret.
     return secret
 }
