a494011239a16796014e374ef2e3c35a1b28c451
Sangam Ghimire <1sangamghimire1@gmail.com>
2023-09-08 16:17:18 +0545

    documentation of signup done

--- src/pages/api/sign_up.ts
+++ src/pages/api/sign_up.ts
@@ -5,21 +5,25 @@ import Token from "../../../lib/modals/Token"
 import crypto from "crypto" 
 import { NextApiRequest, NextApiResponse } from "next";
 
+
+//here we are defining the handler function to handle the request and response for the api of sign_up
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
         try{
+            //here we are getting the email, password, firstName, lastName, dob from the request body
             const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
             const {email, password, firstName, lastName,dob} = req.body as any
 
+            //here we are connecting to the database
             const fullName: String = `${firstName} ${lastName}`;
             if (await dbConnect())
             {
                 console.log("Connection established....");
             }
-    
+            //here we are checking if the email already exists or not
             const countUser = await User.countDocuments({ email });
             console.log(countUser);
-            
+            //if email already exists then we are returning the response with status code 409
             if(countUser) //if email already exists
             {
                 console.log("Duplicate Email!!!")
@@ -33,20 +37,24 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             user.last_name = lastName;
             user.name = fullName;
             user.dob = dob;
-            await user.save()
-
-            const token:any = new Token()
-            token.token = crypto.randomBytes(32).toString("hex")
-            await token.save()
+            await user.save(); //save user in database
+            
+            //here we are creating a new token
+            const token:any = new Token();
+            //here we are creating a new token
+            token.token = crypto.randomBytes(32).toString("hex");
+            await token.save(); //save token in database
 
             const url: any = `${baseUrl}users/${user._id}/verify/${token.token}`
             // console.log(`user has been created: ${user}`)
             // console.log(`token has been created: ${token}`)
             console.log(`url: ${url}`);
+            //here we are sending the response with status code 201
             await sendEmail(user.email,"Verification Mail",url)
-
+            //here we are sending the response with status code 201
             return res.status(201).json({ success: true, data: user })
         }catch(error){
+            //if any error occurs then we are returning the response with status code 500
             return res.status(500).json({ success: false, error })
         }
     }
