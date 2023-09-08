799739ca64793b221fe287e80116b8cc72d3f2d1
Sangam Ghimire <1sangamghimire1@gmail.com>
2023-09-08 16:14:00 +0545

    documentation of forget password done

--- src/pages/api/forgot_password.ts
+++ src/pages/api/forgot_password.ts
@@ -5,15 +5,17 @@ import Token from "../../../lib/modals/Token"
 import crypto from "crypto" 
 import { NextApiRequest, NextApiResponse } from "next";
 
+//here we are defining the handler function to handle the request and response for the api of forgot_password
 export default async function handler(req: NextApiRequest, res: NextApiResponse){
     if (req.method === 'POST') {
         try{
             const { email } = req.body as any;
-
+            //here we are connecting to the database
             if (await dbConnect()){
                 console.log("Connection established....");
             }
 
+            //here we are checking if the email already exists or not
             const user: any = await User.findOne({email: email});
             console.log(user.email);
             if(!user) //if email doesn't exists
@@ -22,15 +24,18 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 return res.status(409).json({ success: false, error:"Unregistered Email" })
             }
 
+            //here we are creating a new token
             const token:any = new Token();
             token.token = crypto.randomBytes(32).toString("hex");
             await token.save();
             
+            //here we are saving the token to the database
             const url: any = `${process.env.BASE_URL}users/${user._id}/reset/${token.token}`;
             console.log(url)
 
             // sending mail via nodemailer.....
-            await sendEmail(user.email,"Reset Your Password",url)
+            await sendEmail(user.email,"Reset Your Password",url);
+            //here we are sending the response with status code 201
             return res.status(201).json({ success: true })
         }catch(error){
             console.log(error)
