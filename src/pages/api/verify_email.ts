0fad1ca6f218dd5095e8ace282d18fa5c17c4fca
Sangam Ghimire <1sangamghimire1@gmail.com>
2023-09-08 16:18:40 +0545

    documentation of verify email done

--- src/pages/api/verify_email.ts
+++ src/pages/api/verify_email.ts
@@ -1,16 +1,18 @@
 import dbConnect from "../../../lib/mongoose"
 import User from "../../../lib/modals/User"
 
+//here we are defining the handler function to handle the request and response for the api of verify_email
 export default async function handler(req: any, res: any){
     if(req.method === 'POST'){
-        const {required_id} =  req.body as any
-        console.log(required_id)
-        const _id = `ObjectId('${required_id}')`
+        const {required_id} =  req.body as any;
+        console.log(required_id);
+        const _id = `ObjectId('${required_id}')`;
         if (await dbConnect())
         {
             console.log("Connection established in verify_email");
         }
-        console.log(_id)
+        console.log(_id);
+        //to check if the user already exists or not
         await User.findOneAndUpdate({"_id" : required_id},{verified : true})
         return res.status(201).json({ success: true })
     }
