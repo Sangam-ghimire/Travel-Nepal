a9ec4cf8b916ef43d0ee4401a81d7b688a6317d4
Sangam Ghimire <1sangamghimire1@gmail.com>
2023-09-08 13:01:42 +0545

    documented the location.ts inside of modals

--- lib/modals/Location.ts
+++ lib/modals/Location.ts
@@ -1,11 +1,13 @@
 import mongoose from 'mongoose'
 
+//interface for Location to be used in the database
 interface ILocation {
     address: string,
     description: string
     registeredTime: mongoose.Schema.Types.Date
 }
 
+//schema for Location to be used in the database using the interface above i.e ILocation
 const locationSchema = new mongoose.Schema<ILocation>({
     address: {
         required: true,
@@ -23,5 +25,6 @@ const locationSchema = new mongoose.Schema<ILocation>({
     }
 })
 
+//for exporting the schema as a model to be used in the database
 const Location = mongoose.models.Location||mongoose.model("Location", locationSchema);
 export default Location;
