09edb61a18c5a7e665011ffc2671937518c1bfe1
Sangam Ghimire <1sangamghimire1@gmail.com>
2023-09-08 16:10:59 +0545

    done documentation of changepassword.ts

--- src/pages/api/change_password.ts
+++ src/pages/api/change_password.ts
@@ -1,6 +1,7 @@
 import dbConnect from "../../../lib/mongoose"
 import User from "../../../lib/modals/User"
 
+//here we are defining the handler function to handle the request and response for the api of verify_email
 export default async function handler(req: any, res: any){
     if(req.method === 'POST'){
         const {required_id, password} =  req.body as any
@@ -11,7 +12,9 @@ export default async function handler(req: any, res: any){
             console.log("Connection established in verify_email");
         }
         console.log(_id)
+        //here we are checking if the email already exists or not
         await User.findOneAndUpdate({"_id" : required_id},{password: password});
+        //if email already exists then we are returning the response with status code 409
         return res.status(201).json({ success: true });
     }
 }
